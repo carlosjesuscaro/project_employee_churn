7ade6c257cdbcb65e8a86f3a320715f642ab9d71
Carlos Caro <carlosjesuscaro@icloud.com>
2020-09-01 12:24:41 -0400

    Data cleaning complete + bug fixes

--- main.R
+++ main.R
@@ -10,39 +10,51 @@ data_raw <- read.csv('Employee Churn.csv')
 ####################################################################
 # DATA PREPARATION PLAN
 ####################################################################
-# 1. Ensureing that all dates are in the format: month/day/year
-# 2. Termination date must be replaced by hire date + length of service
-# 3. Organizing employees based on 3 categorical groups regarding the number
+# 1. Reducing the number of rows as many employees are listed multiple times
+# 2. Ensureing that all dates are in the format: month/day/year
+# 3. Termination date must be replaced by hire date + length of service
+# 4. Organizing employees based on 3 categorical groups regarding the number
 # of years of service
-# 4. Organizing employees based on 3 categorical groups: executives, management
+# 5. Organizing employees based on 3 categorical groups: executives, management
 # and non management
-# 5. Replacing the employee ID for a unique code
-# 6. Calculate information about the age when the employee was hired to verify
-# the data consistency (Min, Max, Mean, Median)
 
 # Data assumptions/corrections
-# 1. The Employee ID column has multiple repeated and it is being assumed
-# that it is an error
+# 1. The Employee ID column has multiple repeated with the same information except
+# for columns 'age' and 'length_of_service'. For purposes of this exercise, it is assumed
+# that the real values are from the first time the employee is listed
 # 2. 'Termination Date' is set as '1/1/1900' to all employees. This is incorrect
 # and it will be recalculated
 
 ####################################################################
 # DATA PREPARATION EXECUTION
 ####################################################################
-# 1. Ensure that all dates are in the format: month/day/year
+
+#1. Reducing the number of rows as many employees are listed multiple time
+temp <- 0
+for (i in 1:length(data_raw$EmployeeID)) {
+  if (temp != data_raw$EmployeeID[i]) {
+    if (i == 1) {data <- data_raw[1,]}
+    else {
+    temp <- data_raw$EmployeeID[i]
+    data <- rbind(data, data_raw[i,])
+    }
+  }
+}
+
+# 2. Ensure that all dates are in the format: month/day/year
 library(tidyverse)
 as_date <- function(x) as.Date(x, format = "%m/%d/%Y")
-data <- data_raw %>% mutate(recorddate_key = as_date(recorddate_key),
+data <- data %>% mutate(recorddate_key = as_date(recorddate_key),
            birthdate_key = as_date(birthdate_key),
            orighiredate_key = as_date(orighiredate_key),
            terminationdate_key = as_date(terminationdate_key))
-# 1.1 Removing the timestamp from recorddate_key
-data$recorddate_key <- format(as.POSIXct(data_raw$recorddate_key, format='%m/%d/%Y %H:%M'), format='%m/%d/%Y')
+# 2.1 Removing the timestamp from recorddate_key
+data$recorddate_key <- format(as.POSIXct(data$recorddate_key, format='%m/%d/%Y %H:%M'), format='%m/%d/%Y')
 
-# 2. Termination date must be replaced by hire date + length of service
+# 3. Termination date must be replaced by hire date + length of service
 data$terminationdate_key <- data$orighiredate_key + data$length_of_service * 365
 
-# 3. Organizing employees based on 3 categorical groups regarding the number
+# 4. Organizing employees based on 3 categorical groups regarding the number
 # of years of service
 # The categories are: A) Short->0~5, Medium->5~15, Long->15+
 # Defining the function that will create the categories
@@ -61,7 +73,7 @@ barplot(table(data$length_categ), xlab = "Categories", ylab = "Number of employe
         legend.text = 'A: Under 5y, B: between 5 and 15y, C: More than 15y')
 title('Length of Service Categories')
 
-# 4. Organizing employees based on 3 categorical groups: executives, management
+# 5. Organizing employees based on 3 categorical groups: executives, management
 # and non management
 emp_categ <- function(x){
   if (x == 'CEO' || grepl('VP', x, ignore.case = TRUE) ||
@@ -82,6 +94,3 @@ for (i in 1:length(data$job_title)) {
 # barplot just to get an idea of the proportion
 barplot(table(data$emp_categ), xlab = "Categories", ylab = "Number of employees")
 title('Job Categories')
-
-
-
